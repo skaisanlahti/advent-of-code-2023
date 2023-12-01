5725703001be912841f652f2c10777ca3cc518ae
Sami Kaisanlahti <skaisanlahti@mailfence.com>
2023-12-01 20:49:54 +0200

    Use flags package

--- day1/cmd/main.go
+++ day1/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
 	"time"
 
 	"github.com/skaisanlahti/advent-of-code-2023/day1/internal/calibrator"
@@ -11,15 +10,9 @@ import (
 
 func main() {
 	start := time.Now()
-	if len(os.Args) < 2 {
-		panic("Usage: go run cmd/part1/main.go <filepath>")
-	}
-
-	filePath := os.Args[1]
+	checkStrings, filePath := reader.ReadFlags()
 	input := reader.ReadInputFile(filePath)
-
-	result := calibrator.Calibrate(input, false)
+	result := calibrator.Calibrate(input, checkStrings)
 	duration := time.Since(start).Nanoseconds()
-
 	log.Printf("Calibrated %s in %d ns with result %d.", filePath, duration, result)
 }
--- day1/internal/reader/reader.go
+++ day1/internal/reader/reader.go
@@ -2,8 +2,21 @@ package reader
 
 import (
 	"bufio"
+	"flag"
 	"os"
 )
+
+func ReadFlags() (bool, string) {
+	checkStrings := flag.Bool("s", false, "Check strings for digit values.")
+	filePath := flag.String("f", "", "Input file path.")
+	flag.Parse()
+
+	if filePath == nil {
+		panic("Usage: go run cmd/main.go -s -f=<filepath>")
+	}
+
+	return *checkStrings, *filePath
+}
 
 func ReadInputFile(filePath string) []string {
 	file, err := os.Open(filePath)
